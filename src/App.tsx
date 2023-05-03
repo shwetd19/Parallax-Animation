5a85226238f69fc0099c59c137b69da98a1430bd
ShwetSd <119885670+shwetd19@users.noreply.github.com>
2023-05-03 18:55:33 +0530

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,137 @@
+import React, { useRef } from 'react'
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+
+// Little helpers ...
+// A helper function that returns a URL string for the given name
+
+const url = (name: string, wrap = false) =>
+  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+
+export default function App() {
+  // useRef returns an object that holds a mutable value (in this case, null!),
+  // which we can use to create a reference to the Parallax component so that
+  // we can call its methods (e.g., scrollTo) later on.
+  const parallax = useRef<IParallax>(null!)
+  // The main React component
+  return (
+    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
+       {/* A Parallax component with three pages */}
+      <Parallax ref={parallax} pages={3}>
+         {/* A dark purple layer that covers the second page */}
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
+          {/* A light blue layer that covers the third page */}
+        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+
+        {/* To use the stars in the complete background of the website */}
+         {/* A layer that shows a starry background */}
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={3}
+          style={{
+            backgroundImage: url('stars', true),
+            backgroundSize: 'cover',
+          }}
+        />
+        {/* An image of a satellite that moves across the page */}
+        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+        </ParallaxLayer>
+
+        {/* A few clouds that move across the page */}
+        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+        </ParallaxLayer>
+
+        {/* A few more clouds */}
+        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+        </ParallaxLayer>
+
+        {/* A few more clouds */}
+        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+        </ParallaxLayer>
+
+        {/* A few more clouds */}
+        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+        </ParallaxLayer>
+
+        {/* A few more clouds */}
+        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+        </ParallaxLayer>
+
+        {/* An image of a Earth that moves across the page (upwards)*/}
+        <ParallaxLayer
+          offset={2.5}
+          speed={-0.4}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            pointerEvents: 'none',
+          }}>
+          <img src={url('earth')} style={{ width: '60%' }} />
+        </ParallaxLayer>
+        
+        {/* An image of a Clients-Screen that moves across the page (upwards)*/}
+        <ParallaxLayer
+          offset={2}
+          speed={-0.3}
+          style={{
+            backgroundSize: '80%',
+            backgroundPosition: 'center',
+            backgroundImage: url('clients', true),
+          }}
+        />
+
+        {/* An image of a Clients-Server that moves across the page (upwards)*/}
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(1)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <img src={url('server')} style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        {/* An image of a Black-Bash that moves across the page (upwards)*/}
+        <ParallaxLayer
+          offset={1}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(2)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <img src={url('bash')} style={{ width: '40%' }} />
+        </ParallaxLayer>
+
+        {/* An image of a Client-Main-Screen that moves across the page (upwards)*/}
+        <ParallaxLayer
+          offset={2}
+          speed={-0}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onClick={() => parallax.current.scrollTo(0)}>
+          <img src={url('clients-main')} style={{ width: '40%' }} />
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  )
+}
